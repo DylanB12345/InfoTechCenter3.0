24013427b9f9f47eacc481551996a20be01b61f6
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-10-03 08:07:25 -0400

    Add print Statements to Welcome Visitors

--- OperationFurry.py
+++ OperationFurry.py
@@ -6,3 +6,7 @@
 Our Welcome Screen will start are our program letting
 drivers know that the infoTechCenter OS is Loading
 """
+
+print("\n\nWelcome to Operation Fury infoTech Center")
+
+print("\n\nOperation Fury's Operating System is Booting Up")
