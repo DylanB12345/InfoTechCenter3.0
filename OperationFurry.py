892e4b6dc818ab4ca1904b65b69f86db801785a0
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-10-04 08:38:04 -0400

    Added color to are program

--- OperationFurry.py
+++ OperationFurry.py
@@ -14,3 +14,8 @@ from time import sleep #We Imported the Sleep funcition from the time libary
 print("\n\nWelcome to Operation Fury infoTech Center")
 sleep(2)
 print("\n\nOperation Fury's Operating System is Booting Up")
+
+print("\033[1;31m This text is Bright Green  \n")
+
+
+print("\033[1;32m This text is Bright Green  \n")
