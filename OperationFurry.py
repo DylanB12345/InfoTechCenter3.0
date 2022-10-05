d62683579596393365f046ecca2994612ad4e4f9
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-10-05 08:39:13 -0400

    Initial Weather Commit

---
+++ OperationFurry.py
@@ -0,0 +1,10 @@
+#Weather
+#developer ; Dylan bellinger
+#version 1.0
+
+""""
+create a function got our current weather using the 
+random. choice function to determine what the weather is 
+picking from a list - using if, elif & else statement 
+to check the condition and print a specific print line
+""""
