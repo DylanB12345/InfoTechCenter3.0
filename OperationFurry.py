ac42604f1651fea5b8209fef06da7ca5233e8620
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-10-06 08:38:20 -0400

    Defind the Weather Function

--- OperationFurry.py
+++ OperationFurry.py
@@ -2,9 +2,24 @@
 #developer ; Dylan bellinger
 #version 1.0
 
-""""
+"""
 create a function got our current weather using the 
 random. choice function to determine what the weather is 
 picking from a list - using if, elif & else statement 
 to check the condition and print a specific print line
-""""
+"""
+
+#Import libraries here
+import random
+
+#Weather Condition list using the random.choice library
+#to randomly choose a condition and storing it in its brain
+def weather ():
+    weather_Forcast = ["Rainy", "Snowy" , "Sunny" , "Cloudy" , "Foggy" , "Stormy" , "Icy"]
+    weather_Condition = random.choice(weather_Forcast)
+    print(weather_Condition)
+    return weather_Condition
+
+
+
+
