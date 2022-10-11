1861825e7666f6b1595df913843c92c01cbacd7d
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-10-11 08:38:34 -0400

    defined Function list_Of_Gas_Stations

--- OperationFurry.py
+++ OperationFurry.py
@@ -19,13 +19,24 @@ def gas_Level_Gauge():
     return currentGasLevel
 
 # Variable calling the gas_Level_Guage function to store value once
-
 gas_Level_Indicator = gas_Level_Gauge()
+
+def list_Of_Gas_Stations():
+    gasStation = ["Shell" , "Circle K" , "Marathon" , "Speedway" , "Meijer"]
+    gas_Station_Nearby = random.choice(gasStation)
+    return gas_Station_Nearby
 
 def gas_Level_Alert():
     if gas_Level_Indicator == "Empty":
         print("***WARNING YOU ARE ON EMPTY***")
         sleep(1.5)
         print("Calling Emergncy Contact")
+    elif gas_Level_Indicator == "Low":
+        print("***WARNING***")
+        sleep(1.5)
+        print("Your gas tank is extremely low, checking google maps for the closest gas station")
+        sleep(1.5)
+        print("The closest gas station is" , list_Of_Gas_Stations())
+
 
 gas_Level_Alert()
